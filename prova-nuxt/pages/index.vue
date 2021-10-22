faeacd5acad39ec46a1e23e5c572497660cb0fa1
MariaClaraGandini <mariaclaragandinipereira175@gmail.com>
2021-10-22 08:52:14 -0300

    prova-nuxt

---
+++ prova-nuxt/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+	<main>
+		<b-container class="pt-4">
+				<b-row>
+					<b-col cols="12">
+						<div>
+							<h1 class= "text-center">
+								Conheça nossa unidade!
+							</h1>
+							<p class="description text-center">
+							Fatec de Matão foi fundada em outubro de 2018, uma das 73 Unidades de Ensino
+							do Centro Estadual de Educação Tecnlológica Paula Souza, que são distribuidas
+							em 67 municípios paulista.
+							</p>
+						</div>
+					</b-col>
+				</b-row>
+					<b-row class="text-center mt-3">
+					<b-col cols="12" lg="4">
+						<div class="m-4">
+							<img src="" alt="Graduação" width="50" class="mt-3">
+							<h2 class="d-inline-flex pt-2 ">Missão</h2>
+
+							<p class="mt-3">
+								Oferecer educação <br>
+								gratuita e de qualidade <br>
+								para formar tecnológos <br>
+								na área da
+								AgroIndústria
+							</p>
+						</div>
+					</b-col>
+
+					<b-col cols="12" lg="4">
+						<div class="m-4">
+							<img src="" alt="Graduação" width="60" class="mt-3">
+							<h2 class="d-inline-flex pt-2">Visão</h2>
+
+							<p class="mt-3">
+							Consolidar-se como <br>
+							referência nacional na <br>
+							formação e capacitação <br>
+							profissional.
+							</p>
+						</div>
+					</b-col>
+
+					<b-col cols="12" lg="4">
+						<div class="m-4">
+							<img src="" alt="Valores da Fatec Matão" width="40" class="mt-3">
+							<h2 class="d-inline-flex pt-2 ">Valores</h2>
